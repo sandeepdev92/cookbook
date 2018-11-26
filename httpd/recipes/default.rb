6bbe2721dbdb12adaa2c27301e69600d4d7a274c
sandeep kumar <sandeepdev92@gmail.com>
2018-11-26 07:21:26 +0000

    add httpd

--- httpd/recipes/default.rb
+++ httpd/recipes/default.rb
@@ -3,3 +3,16 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
