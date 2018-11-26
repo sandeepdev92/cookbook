564141ce273fe2714bc234548644dd0894134251
sandeep kumar <sandeepdev92@gmail.com>
2018-11-26 12:58:24 +0000

    add

---
+++ test/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: httpd
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
